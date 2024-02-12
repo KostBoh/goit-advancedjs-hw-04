41cc4a29972d753e1d781cd7c8dd2f96e9effe53
KostBoh <bohdan.kostya@gmail.com>
2024-02-12 07:17:20 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -22,6 +22,7 @@ export default defineConfig(({ command }) => {
           },
           entryFileNames: 'commonHelpers.js',
         },
+        external: ['iziToast'],
       },
       outDir: '../dist',
     },
